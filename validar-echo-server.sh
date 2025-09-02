30a962e0460dc3944c0bac9b434b23a734c3254b
cindy <cindy1404hsieh@gmail.com>
2025-09-02 13:33:58 -0300

    execute echo inside container

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Pruebo el echo server
 TEST_MSG="Hola mundo"
-RESPONSE=$(echo "$TEST_MSG" | docker run --rm --network tp0_testing_net busybox nc server 12345)
+RESPONSE=$(docker run --rm --network tp0_testing_net busybox sh -c "echo '$TEST_MSG' | nc server 12345")
 
 if [ "$RESPONSE" = "$TEST_MSG" ]; then
     echo "action: test_echo_server | result: success"
